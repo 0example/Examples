1c1f44f380362bb6e3439aaeefba10b33b5ca327
0example.com <79515743+0example@users.noreply.github.com>
2021-02-25 11:52:15 +0530

    Add files via upload

---
+++ Channel Buffering/ChannelBuffering.go
@@ -0,0 +1,20 @@
+/*	Copyright 2021 www.0example.com, powered by Wixis360  */
+
+//By default channels can accept a single send(<-) if there is a recieve. They are unbuffered. 
+//Buffered channels can store multiple values without a corresponding receiver for those values.
+
+package main
+
+import "fmt"
+
+func main() {
+	var c chan string = make(chan string, 3) // creates a buffered channel with the capacity of 3
+
+	c <- "Go"
+	c <- "Lang"
+	c <- "Oexample" //can store multiple values like this in the buffered channel.
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
