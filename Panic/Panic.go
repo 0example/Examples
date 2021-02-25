5ed1c0d6936f8f72f1226fcc482f148e87f0491f
0example.com <79515743+0example@users.noreply.github.com>
2021-02-25 17:08:59 +0530

    Add files via upload

---
+++ Panic/Panic.go
@@ -0,0 +1,24 @@
+/*	Copyright 2021 www.0example.com, powered by Wixis360  */
+
+
+//Panic is a built in mechanism which handles error situations.
+//When the panic function is called, it stops the ordinary flow of the program. 
+//The panic function has a parameter: a message to show.
+//You can use the panic function if an unexpected error arises in your go program
+//that you don’t want or don’t know how to deal with.
+
+
+
+package main
+
+import "os"
+
+func main(){
+	_, err := os.Create("/root/0example")
+	if err != nil {
+		panic("Cannot create file")
+	}
+}
+
+//File is needed to be created and if it cannot create the file 
+//the panic will terminate the function a exit the program after giving a message.
